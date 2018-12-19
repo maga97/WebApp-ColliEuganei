3d64ce546930de982bfb559570eeb5e0ee08880a
Francesco Magarotto <magarotto.francesco@gmail.com>
2018-12-19 09:55:15 +0100

    Editato script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function menuMobile() {
 	document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
-
 }
 
 function clx(str, obj) {
