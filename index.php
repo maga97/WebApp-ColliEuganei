1e27bcf10d88d1c04a6afa235f4a574c6071456c
GiulioPiva <GiulioPiva@outlook.it>
2019-02-13 01:48:03 +0100

    piccoli fix

--- index.php
+++ index.php
@@ -14,8 +14,7 @@ if (session_status() == PHP_SESSION_NONE) { session_start(); }
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" />
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script type="text/javascript" src="js/script.js"></script>
-
-      <script type="text/javascript" src="js/showgallery.js"></script>
+  <script type="text/javascript" src="js/showgallery.js"></script>
 	<title>Home - Colli Digitali</title>
 </head>
 <body>
--- js/showgallery.js
+++ js/showgallery.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
     var box = document.getElementById("galleria");
-    console.log(box.classList);
     box.className = "gallery";
-    console.log(box.classList);
 });
