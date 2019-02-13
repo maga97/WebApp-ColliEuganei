46134f8f60157ac888c67d9a76ab8c0fbfdc2fe3
Davide Zilio <ziliodav97@gmail.com>
2019-02-13 22:16:04 +0100

    sistemati link rel admin

--- PrenotazioneAction.php
+++ PrenotazioneAction.php
@@ -28,10 +28,10 @@ if(isset($_POST["cancellaPrenotazione"])){
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css" media="all">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css" media="handled, screen">
     <link rel="stylesheet" type="text/css" href="assets/css/print.css" media="all">
-    <link rel="stylesheet" type="text/css" href="assets/css/form.css" media="all">
-    <link rel="stylesheet" type="text/css" href="assets/css/mobile.css" media="all">
+    <link rel="stylesheet" type="text/css" href="assets/css/form.css" media="handled, screen">
+    <link rel="stylesheet" type="text/css" href="assets/css/mobile.css" media="handled, screen">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script type="text/javascript" src="js/script.js"></script>
