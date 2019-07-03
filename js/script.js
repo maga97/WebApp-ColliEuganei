92c63e2c8bf70d0ab5aa414a9b7ec2b7889bf6a8
giuliopiva <giuliopiva@outlook.it>
2019-07-03 22:25:18 +0200

    fix menu

--- js/script.js
+++ js/script.js
@@ -1,24 +1,4 @@
-function detectIE() {
-    var ua = window.navigator.userAgent;
 
-    var msie = ua.indexOf('MSIE ');
-    if (msie > 0) {
-        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-    }
-
-    var trident = ua.indexOf('Trident/');
-    if (trident > 0) {
-        var rv = ua.indexOf('rv:');
-        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-    }
-
-    var edge = ua.indexOf('Edge/');
-    if (edge > 0) {
-        return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-    }
-
-    return false;
-}
 function menuMobile() {
     document.getElementById("menuprincipale").classList.toggle("responsive");
 }
@@ -37,16 +17,6 @@ $(document).ready(function () {
     setTimeout(function () {
         $(".success").hide();
     }, 10000);
-    $("#scroll-top-btn").css("display", "none");
-    $(".dropdown, .dropdown-content").hover(function() {
-        if(detectIE()) {
-            $('.dropdown-content').css('display', 'inline-block');
-            $('.dropdown-content li a').css("display", "block");
-        }
-    }, function () {
-        $('.dropdown-content').css('display', 'none');
-        $('.dropdown-content li a').css("display", "none");
-    });
     $("#mobile").click(function (e) {
         e.preventDefault();
         menuMobile();
