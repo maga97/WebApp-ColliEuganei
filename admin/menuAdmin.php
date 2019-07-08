528e945f73cb0734e69f913b9039d1fcfafe1831
giuliopiva <giuliopiva@outlook.it>
2019-07-08 14:01:55 +0200

    fix menu admin

--- admin/menuAdmin.php
+++ admin/menuAdmin.php
@@ -7,52 +7,51 @@
             <li><a href="index.php" tabindex="0">Home</a></li>
         <?php endif; ?>
         <li class="dropdown"><a aria-haspopup="true" tabindex="0">Gestione gite</a>
-            <ul class="dropdown-content" role="menu" id="dropdown-trip">
+            <div class="dropdown-content" role="menu" id="dropdown-trip">
                 <?php if ($nomepagina == "add-trip.php"): ?>
-                    <li class="active"><a href="add-trip.php" tabindex="0" role="menuitem">Aggiungi gita</a></li>
+                    <a class="active" href="add-trip.php" tabindex="0" role="menuitem">Aggiungi gita</a>
                 <?php else: ?>
-                    <li><a href="add-trip.php" tabindex="0" role="menuitem">Aggiungi gita</a>
-                    </li>
+                    <a href="add-trip.php" tabindex="0" role="menuitem">Aggiungi gita</a>
+
                 <?php endif; ?>
                 <?php if ($nomepagina == "select-trip-modify.php"): ?>
-                    <li class="active"><a href="select-trip-modify.php" tabindex="0" role="menuitem">Modifica gita</a>
-                    </li>
+                    <a class="active" href="select-trip-modify.php" tabindex="0" role="menuitem">Modifica gita</a>
+
                 <?php else: ?>
-                    <li><a href="select-trip-modify.php" tabindex="0" role="menuitem">Modifica gita</a>
-                    </li>
+                    <a href="select-trip-modify.php" tabindex="0" role="menuitem">Modifica gita</a>
+
                 <?php endif; ?>
                 <?php if ($nomepagina == "remove-trip.php"): ?>
-                    <li class="active"><a href="remove-trip.php" tabindex="0" role="menuitem">Rimuovi gita</a></li>
+                    <a class="active" href="remove-trip.php" tabindex="0" role="menuitem">Rimuovi gita</a></li>
                 <?php else: ?>
-                    <li><a href="remove-trip.php" tabindex="0" role="menuitem">Rimuovi gita</a>
-                    </li>
+                    <a href="remove-trip.php" tabindex="0" role="menuitem">Rimuovi gita</a>
+
                 <?php endif; ?>
-            </ul>
+            </div>
         </li>
         <li class="dropdown"><a aria-haspopup="true" tabindex="0">Gestione utente</a>
-            <ul class="dropdown-content" role="menu">
+            <div class="dropdown-content" role="menu">
                 <?php if ($nomepagina == "add-admin.php"): ?>
-                    <li class="active"><a href="add-admin.php" tabindex="0" role="menuitem">Aggiungi amministratore</a>
-                    </li>
+                    <a class="active" href="add-admin.php" tabindex="0" role="menuitem">Aggiungi amministratore</a>
+
                 <?php else: ?>
-                    <li><a href="add-admin.php" tabindex="0" role="menuitem">Aggiungi amministratore</a>
-                    </li>
+                    <a href="add-admin.php" tabindex="0" role="menuitem">Aggiungi amministratore</a>
                 <?php endif; ?>
                 <?php if ($nomepagina == "remove-admin.php"): ?>
-                    <li class="active"><a href="remove-admin.php" tabindex="0" role="menuitem">Rimuovi
-                            amministratore</a></li>
+                    <a class="active" href="remove-admin.php" tabindex="0" role="menuitem">Rimuovi
+                            amministratore</a>
                 <?php else: ?>
-                    <li><a href="remove-admin.php" tabindex="0" role="menuitem">Rimuovi amministratore</a>
-                    </li>
+                    <a href="remove-admin.php" tabindex="0" role="menuitem">Rimuovi amministratore</a>
+
                 <?php endif; ?>
-            </ul>
+            </div>
         </li>
         <?php if (isset($_SESSION['username'])): ?>
             <li class="dropdown button-right"><a aria-haspopup="true" tabindex="0">Account</a>
-                <ul class="dropdown-content" role="menu">
-                    <li><a href="view-account-admin.php" tabindex="0" role="menuitem">Impostazioni</a></li>
-                    <li><a href="../PHP/Funzioni_Generali/logout.php" tabindex="0" role="menuitem">Logout</a></li>
-                </ul>
+                <div class="dropdown-content" role="menu">
+                    <a href="view-account-admin.php" tabindex="0" role="menuitem">Impostazioni</a>
+                    <a href="../PHP/Funzioni_Generali/logout.php" tabindex="0" role="menuitem">Logout</a>
+                </div>
             </li>
         <?php else: ?>
             <li><a href="../Accedi.php" tabindex="0">Accedi</a></li>
